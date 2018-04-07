3188ceac905b5c42b59319274d69978d306f96dc
Simon Edwards <sedwards@gmail.com>
2018-04-07 10:49:29 +0100

    Add all files

---
+++ AllEngagementsClient.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, unicode_literals
+
+import os
+import time
+import sys
+import requests
+import collections
+
+
+# Load the file
+
+with open('AllEngagementsByClient.csv', encoding="latin-1") as infile:
+    counts = collections.Counter(l.strip() for l in infile)
+for line, count in counts.most_common():
+    print (line, count, sep=',')
+		
+	
+
---
+++ AllEngagementsCompany.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from __future__ import print_function, unicode_literals
+
+import os
+import time
+import sys
+import requests
+import csv
+import collections
+
+
+# Load the file
+
+with open('AllEngagementsByCompany.csv', encoding="latin-1") as infile:
+    counts = collections.Counter(l.strip() for l in infile)
+for line, count in counts.most_common():
+    print (line, count, sep=',')
+		
+	
+
